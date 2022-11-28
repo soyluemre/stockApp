91e49bf87ffbc75250cd7babc271a8f0028dad38
soyluemre <emresoyludev@gmail.com>
2022-11-28 18:45:19 +0300

    useStock

--- src/pages/Firms.jsx
+++ src/pages/Firms.jsx
@@ -1,17 +1,24 @@
 import axios from "axios";
 import { useEffect } from "react";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchFail, fetchStart, getSuccess } from "../features/stockSlice";
 
 const Firms = () => {
+  const dispatch = useDispatch();
   const { token } = useSelector((state) => state.auth);
   const BASE_URL = "https://14220.fullstack.clarusway.com/";
+
   const getFirms = async () => {
+    const url = "firms";
+    dispatch(fetchStart());
     try {
       const { data } = await axios.get(`${BASE_URL}stock/firms/`, {
         headers: { Authorization: `Token ${token}` },
       });
       console.log(data);
+      dispatch(getSuccess({ data, url }));
     } catch (error) {
+      dispatch(fetchFail());
       console.log(error);
     }
   };
