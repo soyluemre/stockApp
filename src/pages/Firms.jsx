a03064f55612de7a5abfda33d93395a7869b1fff
soyluemre <emresoyludev@gmail.com>
2022-11-28 17:48:42 +0300

    axios instance

--- src/pages/Firms.jsx
+++ src/pages/Firms.jsx
@@ -1,5 +1,26 @@
+import axios from "axios";
+import { useEffect } from "react";
+import { useSelector } from "react-redux";
+
 const Firms = () => {
-  return <div>Firms</div>;
+  const { token } = useSelector((state) => state.auth);
+  const BASE_URL = "https://14220.fullstack.clarusway.com/";
+  const getFirms = async () => {
+    try {
+      const { data } = await axios.get(`${BASE_URL}stock/firms/`, {
+        headers: { Authorization: `Token ${token}` },
+      });
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getFirms();
+  }, []);
+
+  return <div></div>;
 };
 
 export default Firms;
